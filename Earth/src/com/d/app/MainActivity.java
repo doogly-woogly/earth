93cc89f9f05182182e0a6f2e94593e5840683ad2
t09h3r <chris.halden@gmail.com>
2012-10-11 21:15:59 +0100

    imports from the earth exam[ple code

--- Earth/src/com/d/app/MainActivity.java
+++ Earth/src/com/d/app/MainActivity.java
@@ -1,5 +1,14 @@
 package com.d.app;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileChannel.MapMode;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
 import android.app.*;
 import android.os.*;
 import android.view.*;
@@ -7,7 +16,7 @@ import android.widget.*;
 
 public class MainActivity extends Activity
 {
-    /** Called when the activity is first crhchdheated. */
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
 	{
